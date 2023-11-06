cff2acb5ae09ae85c7873e82cf31cc8ddd1363b8
Amézir M <98820043+amezir@users.noreply.github.com>
2023-11-06 22:28:46 +0100

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://chat.amezirmessaoud.fr/", {
+export const socket = io("http://chat.amezirmessaoud.fr", {
     autoConnect: false,
 });
 
