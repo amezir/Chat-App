ed77c52d672b3ad4e405ca26aca91f40a00d7c41
Amézir M <98820043+amezir@users.noreply.github.com>
2023-11-17 12:19:19 +0100

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://chat.amezirmessaoud.fr", {
+export const socket = io("https://mmidev.alwaysdata.net/", {
     autoConnect: false,
 });
 
